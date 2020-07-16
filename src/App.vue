11e54beea3909a8c6d1bca90439e11c6b6e07240
naxoduor <naxoduor7@gmail.com>
2020-07-16 22:38:05 +0300

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddUrl v-on:add-url="addNewUrl"/>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Full Url</th>
+          <th>Shortened url</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-bind:key="url_item.id" v-for="url_item in this.url_list">
+          <td>{{url_item.full}}</td>
+          <td>{{url_item.short}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/layout/Header'
+import AddUrl from './components/AddUrl'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    AddUrl
+  },
+  data() {
+    return {
+      url_list:[]
+    }
+  },
+  methods: {
+    addNewUrl(newUrlItem){
+      const{fullUrl} = newUrlItem
+      axios.post("http://127.0.0.1:5000/shortUrls", 
+      {
+        fullUrl
+      })
+      .then(res=>this.url_list=[...this.url_list, res.data])
+      .catch(err=>console.log(err))
+    }
+
+  },
+  created(){
+    axios.get('http://127.0.0.1:5000/')
+    .then(res=>this.url_list=res.data)
+    .catch(err=>console.log(err))
+
   }
 }
 </script>
@@ -24,5 +64,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btn{
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover{
+  background: #664;
 }
 </style>
---
+++ src/components/AddUrl.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1>Add Url</h1>
+        <form @submit="addUrl">
+            <input type="text" v-model="fullUrl" name="fullUrl" id="" placeholder="Enter the Url to be shortened">
+            <input type="submit" class="btn" value="Submit">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"AddUrl",
+    data(){
+        return {
+            fullUrl:''
+        }
+    },
+    methods:{
+        addUrl(e){
+            e.preventDefault()
+            const newUrlItem={
+                fullUrl:this.fullUrl
+            }
+            this.$emit('add-url',newUrlItem)
+            this.fullUrl=''
+        }
+    },
+    
+}
+</script>
+
+<style scoped>
+form {
+    display: flex;
+}
+input[type="text"] {
+    flex: 10;
+    padding: 5px;
+}
+
+input[type="submit"]{
+    flex: 2;
+}
+
+</style>
