fdce3d9f8eef27a6928341f2bb31fb7b8bbf3abf
naxoduor <naxoduor7@gmail.com>
2020-07-23 10:11:04 +0300

    add test

--- tests/unit/url-list.spec.js
+++ tests/unit/url-list.spec.js
@@ -1,5 +1,6 @@
 import UrlList from "@/components/UrlList.vue"
 import { shallowMount } from "@vue/test-utils"
+var assert = require('assert');
 describe("UrlList.vue", ()=>{
     let url_list=[{"clicks":0,"_id":"5f0ec9076ed34b221f5bd2da","full":"http://www.powerafrica.co.ke/","short":"XyB8EmA0L","__v":0},{"clicks":0,"_id":"5f0f3fa2fd447811b0068366","full":"https://vuejs.org","short":"mQz7lXXFd","__v":0},{"clicks":0,"_id":"5f0f4305f649df1388a2880b","full":"https://www.hackerrank.com/","short":"wyGbvTp68","__v":0},{"clicks":0,"_id":"5f11703c1d17730e4eb82f4c","full":"http://www.twitter.com","short":"ogH-ZPgSA","__v":0}]
     let wrapper;
@@ -12,6 +13,7 @@ describe("UrlList.vue", ()=>{
     
     it("renders", ()=>{
         expect(wrapper.exists()).toBe(true)
+        
     })
     it("does h3 exist", ()=>{
         expect(wrapper.find("h3").text()).toBe("Url List")
